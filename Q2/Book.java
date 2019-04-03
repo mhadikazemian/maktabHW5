3e14cede1aa75943aa7b79b84604bf881af6cad0
kazemian <mkzn94@gmail.com>
2019-04-04 02:38:07 +0430

    Fixes Book class

--- Q2/Book.java
+++ Q2/Book.java
@@ -1,4 +1,20 @@
 package com.maktab.HW5.Q2;
 
-public class Book {
+public abstract class Book {
+    private String title;
+    private double price;
+
+    Book(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public abstract void setPrice();
 }
