3a7a3d03b9428b1575e514b126136dca4561ef37
kazemian <mkzn94@gmail.com>
2019-04-04 01:48:31 +0430

    Adds Address class

--- Q1/Address.java
+++ Q1/Address.java
@@ -1,4 +1,40 @@
 package com.maktab.HW5.Q1;
 
 public class Address {
+    private String state;
+    private String city;
+    private String street;
+    private int ZIPCode;
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public int getZIPCode() {
+        return ZIPCode;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setZIPCode(int ZIPCode) {
+        this.ZIPCode = ZIPCode;
+    }
 }
