472a3f0ca9eaa0b27cf7b0510bcb91078aeb6b77
kazemian <mkzn94@gmail.com>
2019-04-04 02:05:50 +0430

    Adds OnlineStore class

--- Q1/OnlineStore.java
+++ Q1/OnlineStore.java
@@ -1,4 +1,13 @@
 package com.maktab.HW5.Q1;
 
 public class OnlineStore {
+    private User user;
+    private List list;
+    Product product;
+    public void userSignUp(){
+        User user = new User();
+        this.user = user;
+        
+
+    }
 }
