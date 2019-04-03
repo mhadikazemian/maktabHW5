e76c44e9c01310d83b1bf33639661b81ae1f0504
kazemian <mkzn94@gmail.com>
2019-04-04 02:18:15 +0430

    Fixes OnlineStore class

--- Q1/OnlineStore.java
+++ Q1/OnlineStore.java
@@ -1,13 +1,29 @@
 package com.maktab.HW5.Q1;
 
+import java.util.Scanner;
+
 public class OnlineStore {
+    Product product;
+    Scanner scanner = new Scanner(System.in);
     private User user;
     private List list;
-    Product product;
-    public void userSignUp(){
+
+    public void userSignUp() {
         User user = new User();
         this.user = user;
-        
+        System.out.println("Enter your username: ");
+        user.setUserName(scanner.nextLine());
+        System.out.println("Enter your password: ");
+        user.setPassword(scanner.nextInt());
+        System.out.println("Enter your first name: ");
+        user.setFirstName(scanner.nextLine());
+        System.out.println("Enter your last name: ");
+        user.setLastName(scanner.nextLine());
+        System.out.println("Enter your cellphone number: ");
+        user.setCellNumber(scanner.nextInt());
+        System.out.println("Enter your Email address: ");
+        user.setEmail(scanner.next());
+        user.insertAddress();
 
     }
 }
