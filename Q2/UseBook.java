3c71abcd8fe783ceb6906bb652612f6b4379170f
kazemian <mkzn94@gmail.com>
2019-04-04 03:28:38 +0430

    Fixes UseBook class

--- Q2/UseBook.java
+++ Q2/UseBook.java
@@ -1,4 +1,17 @@
 package com.maktab.HW5.Q2;
 
 public class UseBook {
+    public static void main(String[] args) {
+        Fiction fiction = new Fiction("Harry Potter");
+        NonFiction nonFiction = new NonFiction("Database Systems");
+        System.out.println("Fiction book's fields:  ");
+        System.out.println("Title: " + fiction.getTitle());
+        System.out.println("Price: " + fiction.getPrice());
+        System.out.println("*******************");
+        System.out.println("NonFiction book's fields:  ");
+        System.out.println("Title: " + nonFiction.getTitle());
+        System.out.println("Price: " + nonFiction.getPrice());
+    }
+
+
 }
