3485c2e5f85aa4c6191b358c37f242caad21cf5b
kazemian <mkzn94@gmail.com>
2019-04-04 13:15:19 +0430

    Fixes PhoneCall class

--- Q3/PhoneCall.java
+++ Q3/PhoneCall.java
@@ -1,4 +1,20 @@
 package com.maktab.HW5.Q3;
 
-public class PhoneCall {
+public abstract class PhoneCall {
+    String phoneNumber;
+    double price;
+    PhoneCall(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+        this.price = 0.0;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public abstract double getPrice();
+
+    public abstract String getPhoneNumber();
+
+    public abstract void getCallInfo();
 }
