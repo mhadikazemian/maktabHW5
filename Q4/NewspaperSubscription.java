fc6509f1cbc579ed8ea3433593cbcfe0a09c5b29
kazemian <mkzn94@gmail.com>
2019-04-04 17:41:46 +0430

    Fixes NewspaperSubscription class

--- Q4/NewspaperSubscription.java
+++ Q4/NewspaperSubscription.java
@@ -5,4 +5,21 @@ public abstract class NewspaperSubscription {
     private String address;
     private double rate;
 
+    public String getSubscriberName() {
+        return subscriberName;
+    }
+
+    public void setSubscriberName(String subscriberName) {
+        this.subscriberName = subscriberName;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public abstract void setAddress(String address);
 }
