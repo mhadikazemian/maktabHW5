b4f99b07ca894965c5e98f7fb088c8c358a0aa64
kazemian <mkzn94@gmail.com>
2019-04-04 18:52:34 +0430

    Fixes OnlineNewspaperSubscription class

--- Q4/OnlineNewspaperSubscription.java
+++ Q4/OnlineNewspaperSubscription.java
@@ -3,8 +3,10 @@ package com.maktab.HW5.Q4;
 public class OnlineNewspaperSubscription extends NewspaperSubscription {
     @Override
     public void setAddress(String address) {
-        if(address.contains("@"))
+        if(address.contains("@")) {
             setRate(9);
+            this.address = address;
+        }
         else
             System.out.println("Your input Email is invalid...");
     }
