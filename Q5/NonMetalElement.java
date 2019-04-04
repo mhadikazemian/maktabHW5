e88d470ad9606a183a292b389805687cd7e164df
kazemian <mkzn94@gmail.com>
2019-04-04 21:35:16 +0430

    Modifies describeElement method in NonMetalElement class

--- Q5/NonMetalElement.java
+++ Q5/NonMetalElement.java
@@ -7,6 +7,9 @@ public class NonMetalElement extends Element {
     @Override
     public void describeElement() {
         System.out.println("************");
+        System.out.println("Element symbol is: " + this.getSymbol());
+        System.out.println("Element atomic number is: " + this.getAtomicNumber());
+        System.out.println("Element atomic weight is: " + this.getAtomicWeight());
         System.out.println("This is a nonmetal element and nonmetals are poor conductors");
         System.out.println("************");
     }
