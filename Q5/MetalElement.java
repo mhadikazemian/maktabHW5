d6a06e11d4a4745458fadb182ff98d9e3eb94b16
kazemian <mkzn94@gmail.com>
2019-04-04 20:18:53 +0430

    Adds MetalElement class

---
+++ Q5/MetalElement.java
@@ -0,0 +1,10 @@
+package com.maktab.HW5.Q5;
+
+public class MetalElement extends Element {
+    @Override
+    public void describeElement() {
+        System.out.println("************");
+        System.out.println("This is a metal element and metals are good conductors of electricity");
+        System.out.println("************");
+    }
+}
