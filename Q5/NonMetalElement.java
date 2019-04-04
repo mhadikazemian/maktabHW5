647f92bd01cf87d21731379d56cc378de474bd5d
kazemian <mkzn94@gmail.com>
2019-04-04 20:39:47 +0430

    Adds NonMetalElement class

---
+++ Q5/NonMetalElement.java
@@ -0,0 +1,10 @@
+package com.maktab.HW5.Q5;
+
+public class NonMetalElement extends Element {
+    @Override
+    public void describeElement() {
+        System.out.println("************");
+        System.out.println("This is a nonmetal element and nonmetals are poor conductors");
+        System.out.println("************");
+    }
+}
