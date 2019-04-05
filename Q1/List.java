acef3b83b72836b9e82f5b5aea93aeb719b7eef8
kazemian <mkzn94@gmail.com>
2019-04-05 19:39:18 +0430

    Fixes list method

--- Q1/List.java
+++ Q1/List.java
@@ -1,8 +1,11 @@
 package com.maktab.HW5.Q1;
 
 public class List {
-    public void list(){
-        
-    }
+    public static void list(Product product[]) {
+        for (int i = 0; i < product.length; i++) {
+            System.out.println((i + 1) + ". ");
+            product[i].description();
+        }
 
+    }
 }//end of List class
