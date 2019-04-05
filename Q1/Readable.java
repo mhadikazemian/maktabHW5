feb2ea6d405be42dc31268d0663e57f72c856d10
kazemian <mkzn94@gmail.com>
2019-04-05 12:01:53 +0430

    Adds Readable class methods

--- Q1/Readable.java
+++ Q1/Readable.java
@@ -1,6 +1,23 @@
 package com.maktab.HW5.Q1;
 
-public class Readable extends Product {
+public abstract class Readable extends Product {
     private String name;
     private String publication;
-}
+
+    Readable(double price, int quantity, String name, String publication){
+        super(price,quantity);
+        this.name = name;
+        this.publication = publication;
+    }
+    Readable(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPublication() {
+        return publication;
+    }
+}//end of readable class
