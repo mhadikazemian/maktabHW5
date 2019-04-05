7684024ec84095da73ac1d6e8e04455cb8bdb101
kazemian <mkzn94@gmail.com>
2019-04-05 23:58:51 +0430

    Adds comments to Shoes class

--- Q1/Shoes.java
+++ Q1/Shoes.java
@@ -1,11 +1,14 @@
 package com.maktab.HW5.Q1;
 
 public abstract class Shoes extends Product {
+
+    //Shoes class variables:
     private int size;
     private String model;
     private String brand;
     private String color;
 
+    //Shoes class constructors:
     Shoes(double price, int quantity, int size, String model, String brand, String color){
         super(price,quantity);
         this.size = size;
@@ -17,6 +20,7 @@ public abstract class Shoes extends Product {
 
     }
 
+    //Shoes class constructors:
     public int getSize() {
         return size;
     }
@@ -32,4 +36,5 @@ public abstract class Shoes extends Product {
     public String getColor() {
         return color;
     }
+
 }//end of shoes class
