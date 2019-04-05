c8ffa93bab18e6a7f71f764598e07b1a3af1134a
kazemian <mkzn94@gmail.com>
2019-04-05 12:00:33 +0430

    Adds Formal class' instances and methods

--- Q1/Formal.java
+++ Q1/Formal.java
@@ -1,4 +1,29 @@
 package com.maktab.HW5.Q1;
 
-public class Formal {
-}
+public class Formal extends Shoes {
+
+    String material;//this variable define the type of material of the formal shoe
+
+    Formal(double price, int quantity, int size, String model, String brand, String color, String material){
+        super(price, quantity, size, model, brand, color);
+        this.material = material;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    @Override
+    public void description() {
+        System.out.println("This is a formal shoe and its specifications are: ");
+        System.out.println("Brand: " + this.getBrand());
+        System.out.println("Model: " + this.getModel());
+        System.out.println("Material: " + this.getMaterial());
+        System.out.println("Size: " + this.getSize());
+        System.out.println("Color: " + this.getColor());
+        System.out.println("Price: " + this.getPrice());
+        System.out.println("Available quantity: " + this.getQuantity());
+        System.out.println("************************************************");
+
+    }
+}//end of Formal class
