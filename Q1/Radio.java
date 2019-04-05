343c29e587265aff8c10b1ae10931da907e3fa3e
kazemian <mkzn94@gmail.com>
2019-04-06 00:04:02 +0430

    Adds comments to Radio class

--- Q1/Radio.java
+++ Q1/Radio.java
@@ -2,10 +2,12 @@ package com.maktab.HW5.Q1;
 
 public class Radio extends DigitalDevices{
 
+    //Radio class constructor:
     Radio(double price, int quantity, String brand, String model){
         super(price, quantity, brand, model);
     }
 
+    //The following method describes every object of Radio class:
     @Override
     public void description() {
         System.out.println("This is a radio and its specifications are: ");
@@ -14,7 +16,6 @@ public class Radio extends DigitalDevices{
         System.out.println("Price: " + this.getPrice());
         System.out.println("Available quantity: " + this.getQuantity());
         System.out.println("************************************************");
+    }//end of description method
 
-
-    }
 }//end of Radio class
