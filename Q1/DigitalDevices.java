1539884bfbe69b4519b04be8b935dd975cff90d9
kazemian <mkzn94@gmail.com>
2019-04-05 12:01:30 +0430

    Adds DigitalDevices class' methods

--- Q1/DigitalDevices.java
+++ Q1/DigitalDevices.java
@@ -1,7 +1,22 @@
 package com.maktab.HW5.Q1;
 
-public class DigitalDevices extends Product {
+public abstract class DigitalDevices extends Product {
     private String brand;
     private String model;
+    DigitalDevices(double price, int quantity, String brand, String model){
+        super(price,quantity);
+        this.brand = brand;
+        this.model = model;
+    }
+    DigitalDevices(){
 
-}
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}//end of DigitalDevices class
