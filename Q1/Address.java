5dc2852a7d9c5405bbe263dfb4fbaf3bb29c454e
kazemian <mkzn94@gmail.com>
2019-04-06 01:49:56 +0430

    Adds comments to address class

--- Q1/Address.java
+++ Q1/Address.java
@@ -1,40 +1,44 @@
 package com.maktab.HW5.Q1;
 
 public class Address {
+
+    //Address class variables:
     private String state;
     private String city;
     private String street;
-    private int ZIPCode;
+    private String ZIPCode;
 
+    //Address class getters and setters:
     public String getState() {
         return state;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public int getZIPCode() {
-        return ZIPCode;
     }
 
     public void setState(String state) {
         this.state = state;
     }
 
+    public String getCity() {
+        return city;
+    }
+
     public void setCity(String city) {
         this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
     }
 
     public void setStreet(String street) {
         this.street = street;
     }
 
-    public void setZIPCode(int ZIPCode) {
+    public String getZIPCode() {
+        return ZIPCode;
+    }
+
+    public void setZIPCode(String ZIPCode) {
         this.ZIPCode = ZIPCode;
     }
+    
 }//end of address class
