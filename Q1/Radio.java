2ccb9cc1152bbe1592153aac780bbfd273273e07
kazemian <mkzn94@gmail.com>
2019-04-05 12:00:55 +0430

    Adds Radio class' instances and methods

--- Q1/Radio.java
+++ Q1/Radio.java
@@ -1,4 +1,20 @@
 package com.maktab.HW5.Q1;
 
-public class Radio {
-}
+public class Radio extends DigitalDevices{
+
+    Radio(double price, int quantity, String brand, String model){
+        super(price, quantity, brand, model);
+    }
+
+    @Override
+    public void description() {
+        System.out.println("This is a radio and its specifications are: ");
+        System.out.println("Brand: " + getBrand());
+        System.out.println("Model: " + getModel());
+        System.out.println("Price: " + this.getPrice());
+        System.out.println("Available quantity: " + this.getQuantity());
+        System.out.println("************************************************");
+
+
+    }
+}//end of Radio class
