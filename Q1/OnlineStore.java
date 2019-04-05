dfc01d8d5276133e52005f3d740d762345cf73cc
kazemian <mkzn94@gmail.com>
2019-04-06 00:52:07 +0430

    Adds comments to OnlineStore class

--- Q1/OnlineStore.java
+++ Q1/OnlineStore.java
@@ -3,17 +3,21 @@ package com.maktab.HW5.Q1;
 import java.util.Scanner;
 
 public class OnlineStore {
+
     Scanner scanner = new Scanner(System.in);
+
+    //OnlineStore variables:
     private User user;
     private List list;
 
+    //The following method gets user's information:
     public void userSignUp() {
         User user = new User();
         this.user = user;
         System.out.println("Enter your username: ");
         user.setUserName(scanner.nextLine());
         System.out.println("Enter your password: ");
-        user.setPassword(scanner.nextInt());
+        user.setPassword(scanner.nextLine());
         System.out.println("Enter your first name: ");
         user.setFirstName(scanner.nextLine());
         System.out.println("Enter your last name: ");
@@ -23,9 +27,12 @@ public class OnlineStore {
         System.out.println("Enter your Email address: ");
         user.setEmail(scanner.next());
         user.insertAddress();
-    }
-    public void setList(){
+        System.out.println("You've successfully signed up...");
+    }//end of userSignUp class
 
+    //The following method makes a list of every product that is available at the online store:
+    public void setList(Product[] products) {
+        list.list(products);
     }
 
 }//end of OnlineStore class
