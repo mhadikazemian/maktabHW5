61a4221e7f66a6dd591fb5ff4f41e004557057bb
kazemian <mkzn94@gmail.com>
2019-04-05 11:58:43 +0430

    Adds Product class' methods

--- Q1/Product.java
+++ Q1/Product.java
@@ -1,6 +1,26 @@
 package com.maktab.HW5.Q1;
 
-public class Product {
+public abstract class Product {
     private double price;
     private int quantity;
-}
+    Product(double price, int quantity){
+        this.price = price;
+        this.quantity = quantity;
+    }
+    Product(){
+
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public abstract void description();
+}//end of Product class
