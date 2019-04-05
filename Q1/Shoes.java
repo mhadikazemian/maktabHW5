1ace62d3adcddcb6218fb92b01feff05d0ac9626
kazemian <mkzn94@gmail.com>
2019-04-05 12:02:42 +0430

    Adds Shoes class methods

--- Q1/Shoes.java
+++ Q1/Shoes.java
@@ -1,9 +1,35 @@
 package com.maktab.HW5.Q1;
 
-public class Shoes extends Product {
+public abstract class Shoes extends Product {
     private int size;
     private String model;
     private String brand;
     private String color;
 
-}
+    Shoes(double price, int quantity, int size, String model, String brand, String color){
+        super(price,quantity);
+        this.size = size;
+        this.model = model;
+        this.brand = brand;
+        this.color = color;
+    }
+    Shoes(){
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}//end of shoes class
