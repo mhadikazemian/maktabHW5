6a0fd92b8780e01778d6944ac5ecbe4016dc1cb7
kazemian <mkzn94@gmail.com>
2019-04-05 12:50:06 +0430

    Fixes IncommingPhoneCall class

--- Q3/IncommingPhoneCall.java
+++ Q3/IncommingPhoneCall.java
@@ -3,14 +3,17 @@ package com.maktab.HW5.Q3;
 public class IncommingPhoneCall extends PhoneCall{
     IncommingPhoneCall(String phoneNumber){
         super(phoneNumber);
-        this.price = 0.02;
+        setPrice(0.02);
     }
 
     @Override
     public void getCallInfo() {
-        System.out.println("Phone number: " + this.phoneNumber);
-        System.out.println("The rate: " + this.price);
-        System.out.println("The price: " + this.price);
+        System.out.println("Incoming Phone Call: ");
+        System.out.println("Phone number: " + getPhoneNumber());
+        System.out.println("The rate: " + getPrice());
+        System.out.println("The price: " + getPrice());
+        System.out.println("****************************");
     }
-    
+
+
 }
