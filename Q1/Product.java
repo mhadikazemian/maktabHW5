bdbc355c6a82014caaa8da1358c3de68680e26a3
kazemian <mkzn94@gmail.com>
2019-04-06 00:39:13 +0430

    Adds comments to Product class

--- Q1/Product.java
+++ Q1/Product.java
@@ -1,16 +1,22 @@
 package com.maktab.HW5.Q1;
 
 public abstract class Product {
+
+    //Product class variables:
     private double price;
     private int quantity;
-    Product(double price, int quantity){
+
+    //Product class constructors:
+    Product(double price, int quantity) {
         this.price = price;
         this.quantity = quantity;
     }
-    Product(){
+
+    Product() {
 
     }
 
+    //Product class getters and setters:
     public double getPrice() {
         return price;
     }
@@ -22,5 +28,8 @@ public abstract class Product {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
+    //description method that is abstract and describes objects in child classes:
     public abstract void description();
+
 }//end of Product class
