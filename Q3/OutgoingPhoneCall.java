b7cf23e2339e2d36a803eb7974b22d81905a156b
kazemian <mkzn94@gmail.com>
2019-04-05 12:51:23 +0430

    Fixes OutgoingPhoneCall class

--- Q3/OutgoingPhoneCall.java
+++ Q3/OutgoingPhoneCall.java
@@ -5,5 +5,17 @@ public class OutgoingPhoneCall extends PhoneCall {
     OutgoingPhoneCall(String phoneNumber, double callDuration){
         super(phoneNumber);
         this.callDuration = callDuration;
+        setPrice(0.04 * callDuration);
+    }
+
+    @Override
+    public void getCallInfo() {
+        System.out.println("Outgoing Phone Call: ");
+        System.out.println("Phone number: " + getPhoneNumber());
+        System.out.println("The rate per minute: 0.04");
+        System.out.println("Call duration: " + this.callDuration);
+        System.out.println("Total price: " + (0.04 * this.callDuration));
+        System.out.println("****************************");
+
     }
 }
