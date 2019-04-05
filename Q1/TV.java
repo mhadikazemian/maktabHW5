f738ae04e5db3e2187fcf67f9669500d8b6c83ff
kazemian <mkzn94@gmail.com>
2019-04-05 23:36:29 +0430

    Adds comments to TV class

--- Q1/TV.java
+++ Q1/TV.java
@@ -2,17 +2,21 @@ package com.maktab.HW5.Q1;
 
 public class TV extends DigitalDevices {
 
+    //TV class variables:
     double inch;
 
-    TV(double price, int quantity, String brand, String model, double inch){
+    //TV class constructors:
+    TV(double price, int quantity, String brand, String model, double inch) {
         super(price, quantity, brand, model);
         this.inch = inch;
     }
 
+    //TV class getters:
     public double getInch() {
         return inch;
     }
 
+    //The following method describes each object of TV class:
     @Override
     public void description() {
         System.out.println("This is a TV and its specifications are: ");
@@ -22,6 +26,5 @@ public class TV extends DigitalDevices {
         System.out.println("Price: " + this.getPrice());
         System.out.println("Available quantity: " + this.getQuantity());
         System.out.println("************************************************");
-
     }
 }//end of TV class
