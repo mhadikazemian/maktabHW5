9a3e42862a9b6edcabd2b4a75aa36619a3b72d76
kazemian <mkzn94@gmail.com>
2019-04-06 15:48:41 +0430

    Remove getters from address class

--- Q1/Address.java
+++ Q1/Address.java
@@ -8,34 +8,22 @@ public class Address {
     private String street;
     private String ZIPCode;
 
-    //Address class getters and setters:
-    public String getState() {
-        return state;
-    }
+    //Address class setters:
 
     public void setState(String state) {
         this.state = state;
     }
 
-    public String getCity() {
-        return city;
-    }
 
     public void setCity(String city) {
         this.city = city;
     }
 
-    public String getStreet() {
-        return street;
-    }
 
     public void setStreet(String street) {
         this.street = street;
     }
 
-    public String getZIPCode() {
-        return ZIPCode;
-    }
 
     public void setZIPCode(String ZIPCode) {
         this.ZIPCode = ZIPCode;
