133ba8c090ff6c57065d7d8b7657b35007d4781a
kazemian <mkzn94@gmail.com>
2019-04-07 23:33:43 +0430

    Adds comments to NonFiction class

--- Q2/NonFiction.java
+++ Q2/NonFiction.java
@@ -2,13 +2,16 @@ package com.maktab.HW5.Q2;
 
 public class NonFiction extends Book {
 
+    //NonFiction class constructor:
     NonFiction(String title) {
         setTitle(title);
         setPrice();
     }
 
+    // Overriding the abstract method:
     @Override
     public void setPrice() {
         setPrice(37.99);
     }
-}
+
+}// end of NonFiction class
