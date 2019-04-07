c6baf088f5e85627decd4087e6810edd7b04db1c
kazemian <mkzn94@gmail.com>
2019-04-07 23:29:36 +0430

    Adds comments to Fiction class

--- Q2/Fiction.java
+++ Q2/Fiction.java
@@ -2,13 +2,16 @@ package com.maktab.HW5.Q2;
 
 public class Fiction extends Book {
 
+    //Book class constructor:
     Fiction(String title) {
         setTitle(title);
         setPrice();
     }
 
+    //Overriding the abstract method:
     @Override
     public void setPrice() {
         setPrice(24.99);
     }
-}
+
+}//end of Book class
