99f1bad664eb6fdf9932000ad1d91d91be2b6603
kazemian <mkzn94@gmail.com>
2019-04-07 23:23:22 +0430

    Adds comments to BookArray

--- Q2/BookArray.java
+++ Q2/BookArray.java
@@ -2,25 +2,30 @@ package com.maktab.HW5.Q2;
 
 public class BookArray {
     public static void main(String[] args) {
+
+        //Creating new Book array:
         Book[] arr = new Book[10];
-        for (int i = 0; i < 10 ; i++) {
+
+        //Assigning an object to every element of created array:
+        for (int i = 0; i < 10; i++) {
+
+            //Choosing the type of objects randomly:
             int r = (int) (Math.random() * 2) + 1;
+            
             if (r == 1) {
                 arr[i] = new Fiction("Fiction");
                 System.out.println("Fiction book's fields:  ");
-                System.out.println("Title: " + arr[i].getTitle() + (i+1));
+                System.out.println("Title: " + arr[i].getTitle() + (i + 1));
                 System.out.println("Price: " + arr[i].getPrice());
                 System.out.println("*******************");
             }
-            if (r == 2){
+            if (r == 2) {
                 arr[i] = new NonFiction("NonFiction");
                 System.out.println("NonFiction book's fields:  ");
-                System.out.println("Title: " + arr[i].getTitle() + (i+1));
+                System.out.println("Title: " + arr[i].getTitle() + (i + 1));
                 System.out.println("Price: " + arr[i].getPrice());
                 System.out.println("*******************");
             }
-
-
-        }
-    }
-}
+        }//end of for loop
+    }//end of main method
+}//end of BookArray class
