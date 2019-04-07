304afdfc84cd66f2872c4d5acacf2decaaccf14e
kazemian <mkzn94@gmail.com>
2019-04-08 01:00:28 +0430

    Adds comments to PhysicalNewspaperSubscription class

--- Q4/PhysicalNewspaperSubscription.java
+++ Q4/PhysicalNewspaperSubscription.java
@@ -1,16 +1,16 @@
 package com.maktab.HW5.Q4;
 
 public class PhysicalNewspaperSubscription extends NewspaperSubscription {
+
+    //Overriding abstract method:
     @Override
     public void setAddress(String address) {
-        if(address.length() < 1){
+        if (address.length() < 1) {
             System.out.println("Invalid address!");
             this.setRate(0);
-        }
-        else {
+        } else {
             this.setRate(15);
             this.address = address;
         }
-
     }
-}
+}//end of PhysicalNewspaperSubscription class
