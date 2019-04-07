1afe15e64cda13ed8b21b0b54308e3feaca6df2e
kazemian <mkzn94@gmail.com>
2019-04-08 00:16:17 +0430

    Adds comments to PhoneCallArray

--- Q3/PhoneCallArray.java
+++ Q3/PhoneCallArray.java
@@ -3,22 +3,31 @@ package com.maktab.HW5.Q3;
 import java.util.Scanner;
 
 public class PhoneCallArray {
+
     public static void main(String[] args) {
+
+        //Creating new PhoneCall array:
         PhoneCall[] phoneCalls = new PhoneCall[8];
+
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 8 ; i++) {
-            int r = (int)((Math.random() * 2) + 1);
-            if(r == 1){
+
+        //Getting phone numbers and durations from user:
+        for (int i = 0; i < 8; i++) {
+
+            //Choosing phone call types randomly:
+            int r = (int) ((Math.random() * 2) + 1);
+
+            if (r == 1) {
                 System.out.println("Enter the incoming phone number: ");
                 phoneCalls[i] = new IncommingPhoneCall(scanner.next());
                 phoneCalls[i].getCallInfo();
             }
-            if(r == 2){
+            if (r == 2) {
                 System.out.println("Enter the out going phone number and call duration: ");
-                phoneCalls[i] = new OutgoingPhoneCall(scanner.next(),scanner.nextDouble());
+                phoneCalls[i] = new OutgoingPhoneCall(scanner.next(), scanner.nextDouble());
                 phoneCalls[i].getCallInfo();
             }
-
         }
-    }
-}
+
+    }//end of main method
+}//end of PhoneCallArray class
