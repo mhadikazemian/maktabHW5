991fa247fc697bcd6c4123737d76ac3793bfaec1
kazemian <mkzn94@gmail.com>
2019-04-08 00:18:45 +0430

    Adds comments to DemoPhoneCalls class

--- Q3/DemoPhoneCalls.java
+++ Q3/DemoPhoneCalls.java
@@ -1,14 +1,17 @@
 package com.maktab.HW5.Q3;
 
 public class DemoPhoneCalls {
+
     public static void main(String[] args) {
+
+        //Creating two new objects:
         PhoneCall incomingPhoneCall = new IncommingPhoneCall("02188880000");
         PhoneCall outgoingPhoneCall = new OutgoingPhoneCall("02166660000", 5);
+
+        //Showing calls info in the output:
         incomingPhoneCall.getCallInfo();
         outgoingPhoneCall.getCallInfo();
-    }
 
+    }//end of main method
 
-
-
-}
+}//end of DemoPhoneCalls class
